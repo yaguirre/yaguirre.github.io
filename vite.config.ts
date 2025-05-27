d8af66f6de67e552f763cb80d203ef961398df10
Yorman Aguirre <yaguirrem01@gmail.com>
2025-05-26 21:53:50 -0500

    fix: updated site base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/webpage/',
+  base: '/yaguirre.github.io/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
